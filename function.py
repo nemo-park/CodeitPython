b3adce3e824110bae864534435f09400955b5302
nemo-park <pklemontree@gmail.com>
2022-02-20 16:54:19 +0900

    Commit and Push

---
+++ function.py
@@ -0,0 +1,16 @@
+# 추상화 (Abstration)
+# 복잡한 내용은 숨기고 주요 기능에만 신경쓰면 됨
+# 함수 (Function) : 명령어의 복잡한 디테일을 알 필요# 추상화 (Abstration)
+# 복잡한 내용은 숨기고 주요 기능에만 신경쓰면 됨없이 주요기능에만 집중
+
+burger_price = 4990
+# 내장함수 : 기본 제공되는 함수
+# ex) print
+print(burger_price)
+
+# Function Header
+def hello():
+    print("Hello!")
+    print("Welcome to Codeit!")
+
+hello()
