f1dc2ced4f869f2d7bf6d3d7fded6eae4b9297c1
nemo-park <pklemontree@gmail.com>
2022-02-20 16:45:32 +0900

    Commit and Push

---
+++ variable.py
@@ -0,0 +1,12 @@
+# 추상화 (Abstration)
+# 복잡한 내용은 숨기고 주요 기능에만 신경쓰면 됨
+# 변수 (Variable) : 값 저장
+
+burger_price = 4750
+fries_price = 1490
+drink_price = 1250
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 3 + fries_price * 2 + drink_price * 5)
