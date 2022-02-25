e5e6e0b90239df9fe34103a0c62fd810d7717262
nemo-park <pklemontree@gmail.com>
2022-02-25 22:14:03 +0900

    자료형 - 문자

---
+++ string_datatype.py
@@ -0,0 +1,12 @@
+##### 자료형
+# 03. 문자열
+
+print("Python")
+
+print("I'm excited to learn Python!")
+
+print("I\'m \"excited\" to learn Python!")
+
+print("Hello" + " World")
+print("Hello " * 3)
+print("3" + "5")
