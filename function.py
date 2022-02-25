051e9b2ddf69c931c5493cd012ec9b5cc909a8d3
nemo-park <pklemontree@gmail.com>
2022-02-25 20:54:04 +0900

    프로그래밍 기초

--- function.py
+++ function.py
@@ -1,16 +1,18 @@
 # 추상화 (Abstration)
 # 복잡한 내용은 숨기고 주요 기능에만 신경쓰면 됨
-# 함수 (Function) : 명령어의 복잡한 디테일을 알 필요# 추상화 (Abstration)
-# 복잡한 내용은 숨기고 주요 기능에만 신경쓰면 됨없이 주요기능에만 집중
+# 함수 (Function) : 명령 저장
 
 burger_price = 4990
-# 내장함수 : 기본 제공되는 함수
-# ex) print
+# 내장함수 : 기본 제공되는 함수 ex) print
 print(burger_price)
 
 # Function Header
-def hello():
+# 파라미터 (Parameter)
+#   함수에 넘겨주는 값
+#   함수에 파라미터를 넘겨주면, 파라미터에 따라 함수가 조금씩 다르게 동작하도록 할 수 있다
+def hello(name):
     print("Hello!")
+    print(name)
     print("Welcome to Codeit!")
 
-hello()
+hello("Nemo")
