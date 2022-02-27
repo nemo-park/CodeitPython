b524f149e6572d57fa7e37e1c98cfd2284e7ec23
nemo-park <pklemontree@gmail.com>
2022-02-27 22:37:35 +0900

    range

--- 03_02_for.py
+++ 03_02_for.py
@@ -18,6 +18,12 @@ print()
 for i in range(10):
     print(i)
 
+numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+# 인덱스와 원소 출력
+for i in range(len(numbers)):
+    print(i, numbers[i])
+
 print()
 
 # 파라미터 3개 버전
@@ -27,3 +33,20 @@ print()
 
 for i in range(3, 17, 3):   # 3 ~ 16
     print(i)
+
+
+# 리스트 뒤집기
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+# 리스트 뒤집기
+for left in range(len(numbers)):
+    # 인덱스 left와 대칭인 인덱스 right 계산
+    right = len(numbers) - left - 1
+
+    # 위치 바꾸기
+    temp = numbers[left]
+    numbers[left] = numbers[right]
+    numbers[right] = temp
+
+print("뒤집어진 리스트: " + str(numbers))
